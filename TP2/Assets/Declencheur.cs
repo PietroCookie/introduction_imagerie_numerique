0aa99c43d3d54a0dffd1bcf5965ddeae0b465b48
PietroCookie <pierrechemin08200@gmail.com>
2023-03-10 22:18:52 +0100

    Ajout TP2

---
+++ TP2/Assets/Declencheur.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Declencheur : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Declenché");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+}
